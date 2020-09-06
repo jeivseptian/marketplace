d7e085876568fc7ce1fbc99529789fd75bb8536c
jeivseptian <septianrahmat459@gmail.com>
2020-09-06 14:58:37 +0700

    init

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,323 @@
+@extends('layouts.app')
+
+@section('title')
+    Store Home Page
+@endsection
+
+@section('content')
+    <div class="page-content page-home">
+      <section class="store-carousel">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12" data-aos="zoom-in">
+              <div
+                id="storeCarousel"
+                class="carousel slide"
+                data-ride="carousel"
+              >
+                <ol class="carousel-indicators">
+                  <li
+                    class="active"
+                    data-target="#storeCarousel"
+                    data-slide-to="0"
+                  ></li>
+                  <li data-target="#storeCarousel" data-slide-to="1"></li>
+                  <li data-target="#storeCarousel" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img
+                      src="/images/banner.jpg"
+                      alt="Carousel Image"
+                      class="d-block w-100"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/banner.jpg"
+                      alt="Carousel Image"
+                      class="d-block w-100"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/banner.jpg"
+                      alt="Carousel Image"
+                      class="d-block w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="store-trend-categories">
+        <div class="container">
+          <div class="row">
+            <div class="col-12" data-aos="fade-up">
+              <h5>Trend Categories</h5>
+            </div>
+          </div>
+          <div class="row">
+            <div
+              class="col-6 col-md-3 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img src="/images/categories.svg" alt="" class="w-100" />
+                </div>
+                <p class="categories-text">
+                  Gadgets
+                </p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-3 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img src="/images/furniture.svg" alt="" class="w-100" />
+                </div>
+                <p class="categories-text">
+                  Furniture
+                </p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-3 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="300"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img src="/images/makeup.svg" alt="" class="w-100" />
+                </div>
+                <p class="categories-text">
+                  Make Up
+                </p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-3 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img src="/images/sneaker.svg" alt="" class="w-100" />
+                </div>
+                <p class="categories-text">
+                  Sneaker
+                </p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-3 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="500"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img src="/images/tools.svg" alt="" class="w-100" />
+                </div>
+                <p class="categories-text">
+                  Tools
+                </p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-3 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="600"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img src="/images/baby.svg" alt="" class="w-100" />
+                </div>
+                <p class="categories-text">
+                  Baby
+                </p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="store-new-products">
+        <div class="container">
+          <div class="row">
+            <div class="col-12" data-aos="fade-up">
+              <h5>New Products</h5>
+            </div>
+          </div>
+          <div class="row">
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/applewatch.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Apple Watch 4
+                </div>
+                <div class="products-price">
+                  $898
+                </div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/orangebogota.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Orange Bogotta
+                </div>
+                <div class="products-price">
+                  $15.000
+                </div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="300"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/sofa.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Sofa
+                </div>
+                <div class="products-price">
+                  $1.409
+                </div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/bubukmaketi.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Bubuk Maketti
+                </div>
+                <div class="products-price">
+                  $250
+                </div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="500"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/tatakangelas.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Tatakan Gelas
+                </div>
+                <div class="products-price">
+                  $200
+                </div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="600"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/dronemavic.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Drone Mavic
+                </div>
+                <div class="products-price">
+                  $500
+                </div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="700"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/sneakerblack.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Nike Run Black
+                </div>
+                <div class="products-price">
+                  $900
+                </div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="800"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="background-image: url('/images/monkeytoys.jpg');"
+                  ></div>
+                </div>
+                <div class="product-text">
+                  Monkey Toys
+                </div>
+                <div class="products-price">
+                  $90
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+@endsection
