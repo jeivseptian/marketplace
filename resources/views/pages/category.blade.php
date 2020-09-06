8b36614e553fb32a4d2ec10bcf3d3ba864717ea7
jeivseptian <septianrahmat459@gmail.com>
2020-09-06 17:21:22 +0700

    konversi HTML ke Laravel Blade

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -1,65 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+@extends('layouts.app')
 
-    <title>Store - Your Best Marketplace</title>
+@section('title')
+    Store Category Page
+@endsection
 
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="style/main.css" rel="stylesheet" />
-  </head>
-
-  <body>
-    <nav
-      class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
-      data-aos="fade-down"
-    >
-      <div class="container">
-        <a href="/index.html" class="navbar-brand">
-          <img src="/images/logo.svg" alt="logo" />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarResponsive"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a href="/index.html" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item">
-              <a href="/categories.html" class="nav-link">Categories</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">Rewards</a>
-            </li>
-            <li class="nav-item">
-              <a href="/resgister.html" class="nav-link">Sign Up</a>
-            </li>
-            <li class="nav-item">
-              <a
-                href="/login.html"
-                class="nav-link btn btn-success px-4 text-white"
-                >Sign In</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <!-- page content -->
-    <div class="page-content page-home">
+@section('content')
+<div class="page-content page-home">
       <section class="store-trend-categories">
         <div class="container">
           <div class="row">
@@ -284,24 +230,4 @@
         </div>
       </section>
     </div>
-
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-12 text-center">
-            <p class="pt-4 pb-2">2020 Copyright Store. All Rights Reserved</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="/vendor/jquery/jquery.slim.min.js"></script>
-    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script>
-      AOS.init();
-    </script>
-    <script src="/script/navbar-scroll.js"></script>
-  </body>
-</html>
+@endsection
